f4b596106f727f8bdef582fe2fe797284a4bb674
pichu2707 <pichu_2707@hotmail.com>
2026-08-09 20:55:02 +0200

    subiendo otro ejercicios de test completo

--- 03-gifs-app/src/counter/MycounterApp2.test.tsx
+++ 03-gifs-app/src/counter/MycounterApp2.test.tsx
@@ -1,7 +1,40 @@
-import { describe, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { MyCounterApp } from "./MyCounterApp";
+import { useCounter } from "./hooks/useCounter";
+
+const handleAddMock = vi.fn();
+const handleSubtractMock = vi.fn();
+const handleResetMock = vi.fn();
+
+vi.mock("./hooks/useCounter", () => ({
+  useCounter: () => ({
+    counter: 5,
+    handleAdd: handleAddMock,
+    handleReset: handleResetMock,
+    handleSubtract: handleSubtractMock,
+  }),
+}));
 
 describe("MyCounterApp", () => {
   test("should render the component", () => {
-    render;
+    render(<MyCounterApp />);
+
+    expect(screen.getByRole("heading", { level: 1 }).innerHTML).toContain(
+      `Counter: 5`,
+    );
+  });
+
+  test("should call handleAdd if button is clicked", () => {
+    render(<MyCounterApp />);
+
+    const buttton = screen.getByRole("button", { name: "+1" });
+
+    fireEvent.click(buttton);
+
+    expect(handleAddMock).toHaveBeenCalled();
+    expect(handleAddMock).toHaveBeenCalledTimes(1);
+    expect(handleSubtractMock).not.toHaveBeenCalled();
+    expect(handleResetMock).not.toHaveBeenCalled();
   });
 });
